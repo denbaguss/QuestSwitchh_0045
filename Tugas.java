f0428895a795b0c49e56a5f3c678b11b217362f5
denbaguss <awanhexom@gmail.com>
2025-11-05 21:36:15 +0700

    menambahkan display program

--- Tugas.java
+++ Tugas.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Tugas {
     public static void main(String[] args) {
-        
+        Scanner raden = new Scanner(System.in);
+        double sData, pbo, bData, algo, rpl;
+
+        System.out.println("Program Seleksi ASDOS");
     }
 }
